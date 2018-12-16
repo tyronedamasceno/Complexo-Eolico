a0df485ed0f2eb3501eba2dc100185822a399fe6
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-12-16 09:35:55 -0300

    Instanciação automática ao rodar

--- src/main/java/com/tyrone/ComplexoEolicoApplication.java
+++ src/main/java/com/tyrone/ComplexoEolicoApplication.java
@@ -1,13 +1,31 @@
 package com.tyrone;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ComplexoEolicoApplication {
+import com.tyrone.domain.ComplexoEolico;
+import com.tyrone.repositories.ComplexoEolicoRepository;
 
+@SpringBootApplication
+public class ComplexoEolicoApplication implements CommandLineRunner{
+
+	@Autowired
+	private ComplexoEolicoRepository complexoEolicoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ComplexoEolicoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ComplexoEolico c1 = new ComplexoEolico(null, "Complexo 1", "RN", "CE1");
+		ComplexoEolico c2 = new ComplexoEolico(null, "Complexo 2", "CE", "CE2");
+		
+		complexoEolicoRepository.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
