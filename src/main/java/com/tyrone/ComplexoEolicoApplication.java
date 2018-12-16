2793136fb283a51cbbf92f3d0be9f6a78cfd7c2d
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-12-16 08:43:53 -0300

    Initial commit

---
+++ src/main/java/com/tyrone/ComplexoEolicoApplication.java
@@ -0,0 +1,14 @@
+package com.tyrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexoEolicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexoEolicoApplication.class, args);
+	}
+
+}
+
