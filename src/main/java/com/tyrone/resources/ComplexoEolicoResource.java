78e21b351c21329d2ee0c92e4c2eabc44a985484
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-12-16 11:25:47 -0300

    CRUD Complexo Eolico completo

--- src/main/java/com/tyrone/resources/ComplexoEolicoResource.java
+++ src/main/java/com/tyrone/resources/ComplexoEolicoResource.java
@@ -1,11 +1,16 @@
 package com.tyrone.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tyrone.domain.ComplexoEolico;
 import com.tyrone.services.ComplexoEolicoService;
@@ -18,11 +23,45 @@ public class ComplexoEolicoResource {
 	private ComplexoEolicoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> findById(@PathVariable Integer id) {
-		
+	public ResponseEntity<ComplexoEolico> findById(@PathVariable Integer id) {
 		ComplexoEolico obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ComplexoEolico>> findAll() {
+		List<ComplexoEolico> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody ComplexoEolico obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody ComplexoEolico obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+
+
+
+
+
+
+
+
 }
+
--- src/main/java/com/tyrone/services/ComplexoEolicoService.java
+++ src/main/java/com/tyrone/services/ComplexoEolicoService.java
@@ -1,12 +1,15 @@
 package com.tyrone.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tyrone.domain.ComplexoEolico;
 import com.tyrone.repositories.ComplexoEolicoRepository;
+import com.tyrone.services.exceptions.DataIntegrityException;
 import com.tyrone.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +23,35 @@ public class ComplexoEolicoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
 				+ ", Tipo: " + ComplexoEolico.class.getName()));
 	}
+	
+	public List<ComplexoEolico> findAll() {
+		return repository.findAll();
+	}
+	
+	public ComplexoEolico insert(ComplexoEolico obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public ComplexoEolico update(ComplexoEolico obj) {
+		ComplexoEolico newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível um Complexo eólico que possui parques eólicos");
+		}
+	}
+	
+	private void updateData(ComplexoEolico newObj, ComplexoEolico obj) {
+		if (obj.getNome() != null) newObj.setNome(obj.getNome());
+		if (obj.getIdentificador() != null) newObj.setIdentificador(obj.getIdentificador());
+		if (obj.getUf() != null) newObj.setUf(obj.getUf());
+	}
+	
 }
