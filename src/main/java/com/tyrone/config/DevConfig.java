ca33af265bb68a856309f8d1e27dcc88fa957ca7
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-12-17 15:04:58 -0300

    Profiles de teste, desenvolvimento e produção

---
+++ src/main/java/com/tyrone/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.tyrone.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.tyrone.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() {
+		
+		if (strategy.equals("create")) {
+			dbService.instantiateTestDatabase();
+			return true;
+		}
+		return false;
+	}	
+}
+
