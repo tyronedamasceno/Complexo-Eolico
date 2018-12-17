3e482279a6b01fee85a35da88fea517d137adcfe
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-12-17 17:59:29 -0300

    Remove GETs de usuario

--- src/main/java/com/tyrone/resources/UsuarioResource.java
+++ src/main/java/com/tyrone/resources/UsuarioResource.java
@@ -1,11 +1,9 @@
 package com.tyrone.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,18 +19,6 @@ public class UsuarioResource {
 	
 	@Autowired
 	private UsuarioService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
-		Usuario obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-	}
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<Usuario>> findAll() {
-		List<Usuario> lista = service.findAll();
-		return ResponseEntity.ok().body(lista);
-	}
 	
 	@RequestMapping(value="sign-up", method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Usuario obj) {
